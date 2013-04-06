e29f6ce0f1d0e7b941d383dea1a49eef25fbd969
t2t2 <taavot@gmail.com>
2013-04-07 01:42:37 +0300

    Move stuff from local to global

--- app/start/global.php
+++ app/start/global.php
@@ -48,6 +48,8 @@ Log::useDailyFiles(storage_path().'/logs/'.$logFile);
 |
 */
 
+
+
 App::error(function(Exception $exception, $code)
 {
 	Log::error($exception);
@@ -65,3 +67,18 @@ App::error(function(Exception $exception, $code)
 */
 
 require __DIR__.'/../filters.php';
+
+App::missing(function($exception) {
+	$layout = View::make("layout.main");
+	$layout->content = View::make("errors.404");
+	return Response::make($layout, 404);
+});
+
+View::composer("layout.main", function ($view) {
+	if(!isset($view->javascript)) {
+		$view->javascript = array();
+	}
+	if(!isset($view->content)) {
+		$view->content = "";
+	}
+});
--- app/start/local.php
+++ app/start/local.php
@@ -1,16 +1,2 @@
 <?php
 
-App::missing(function($exception) {
-	$layout = View::make("layout.main");
-	$layout->content = View::make("errors.404");
-	return Response::make($layout, 404);
-});
-
-View::composer("layout.main", function ($view) {
-	if(!isset($view->javascript)) {
-		$view->javascript = array();
-	}
-	if(!isset($view->content)) {
-		$view->content = "";
-	}
-});
