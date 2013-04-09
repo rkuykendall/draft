350190f886e728762c8a511c4fb5d8d5008a9a9a
t2t2 <taavot@gmail.com>
2013-04-09 19:08:45 +0300

    Extra wording on home page

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -6,9 +6,16 @@
 				<li><a href="{{ action("LeagueController@getView", array("id" => $league->id, "slug" => $league->slug)) }}">{{{ $league->name }}}</a></li>
 			@endforeach
 		</ol>
-   </div>
+	</div>
 	<div class="span4">
 		<h1>DISCLAIMER DISCLAIMER DISCLAIMER</h1>
 		<p>This is alpha. Really alpha. Like it might break at any second. You've been warned.</p>
+		<h2>Create your own league</h2>
+		@if(Auth::check())
+		<p>Gather up your friends and <a href="{{ action("LeagueController@getCreate") }}">create your own league</a>!</p>
+		@else
+		<p>First we gotta know who you are! Log in using your e-mail from above!</p>
+		<p>Why do we use persona? <a href="https://login.persona.org/about" target="_blank">Cause it's pretty cool.</a></p>
+		@endif
 	</div>
 </div>
